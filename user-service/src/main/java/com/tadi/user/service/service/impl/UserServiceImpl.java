ec1ae54542b46fab96c50002a55f6f6e37ca4cbc
Feliz <feliz.zachary@advotics.com>
2022-07-04 17:46:03 +0700

    add docker compose

--- user-service/src/main/java/com/tadi/user/service/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/tadi/user/service/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.tadi.user.service.service.impl;
 
 import com.tadi.dto.user.CreateUserRequest;
+import com.tadi.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,9 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class UserServiceImpl {
+
+    @Autowired
+    private UserRepository userRepository;
 
     public boolean create(CreateUserRequest baseUserRequest) throws Exception {
         return true;
